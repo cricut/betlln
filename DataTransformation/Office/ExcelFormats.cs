a6323b7d825e397c9a891115129f043cd03c2948
Nate Irvin <nate.irvin@gmail.com>
2019-06-05 23:02:37 -0600

    updating Excel formats

--- DataTransformation/Office/ExcelFormats.cs
+++ DataTransformation/Office/ExcelFormats.cs
@@ -2,7 +2,8 @@
 {
     public class ExcelFormats
     {
-        public const string AccountingFormat = "_($* #,##0.00_);_($* (#,##0.00);_($* \"-\"??_);_(@_)";
-        public const string PercentFormat = "0.00%";
+        public const string Accounting = "_($* #,##0.00_);_($* (#,##0.00);_($* \"-\"??_);_(@_)";
+        public const string Percent = "0.00%";
+        public const string IntegerWithDelimiters = "_(* #,##0_);_(* (#,##0);_(* \"-\"??_);_(@_)";
     }
 }
