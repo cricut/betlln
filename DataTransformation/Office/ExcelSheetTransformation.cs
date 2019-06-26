4596846025b1ce8d4f41713fe7f7946ac019601a
Nate Irvin <nate.irvin@gmail.com>
2019-06-26 17:12:08 -0600

    revising Excel tasks

---
+++ DataTransformation/Office/ExcelSheetTransformation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Betlln.Data.Integration.Core;
+
+namespace Betlln.Data.Integration.Office
+{
+    public class ExcelSheetTransformation
+    {
+        private readonly Dictionary<string, ExcelColumn> _columnSettings;
+
+        public ExcelSheetTransformation()
+        {
+            _columnSettings = new Dictionary<string, ExcelColumn>();
+            WriteEmptyTable = true;
+        }
+
+        public DataFeed DataSource { get; set; }
+        public string DestinationSheetName { get; set; }
+        public bool WriteEmptyTable { get; set; }
+
+        public IEnumerable<string> CustomColumnLetters
+        {
+            get { return _columnSettings.Keys; }
+        }
+
+        public ExcelColumn Column(string columnLetter)
+        {
+            columnLetter = columnLetter.ToUpper().Trim();
+            if (!_columnSettings.ContainsKey(columnLetter))
+            {
+                _columnSettings.Add(columnLetter, new ExcelColumn());
+            }
+
+            return _columnSettings[columnLetter];
+        }
+    }
+}
