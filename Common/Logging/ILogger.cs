7ed65c9b3e2e71315c45ad3fc44d4f9ddc361298
Nate Irvin <nate.irvin@gmail.com>
2019-09-03 15:58:21 -0600

    adding interface

---
+++ Common/Logging/ILogger.cs
@@ -0,0 +1,11 @@
+﻿namespace Betlln.Logging
+{
+    public interface ILogger
+    {
+        void Debug(string message);
+        void Info(string message);
+        void Error(string message);
+        void Log(string message, LogEventType eventType = LogEventType.Debug);
+        void Log(LogEntry entry);
+    }
+}
