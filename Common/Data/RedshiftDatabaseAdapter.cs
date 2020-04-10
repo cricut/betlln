ac8f3533bb7e49fd8ec2d3e13a0e4928717c13dc
dwoodruff <dwoodruff@cricut.com>
2020-04-10 12:01:04 -0600

    add redshift adapter and upgrade to 4.7.2 for the framework. 4.6.1 or greater is needed and the code was on 4.5

---
+++ Common/Data/RedshiftDatabaseAdapter.cs
@@ -0,0 +1,104 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+
+namespace Betlln.Data
+{
+    public abstract class RedshiftDatabaseAdapter
+    {
+        protected RedshiftDatabaseAdapter(string connectionName)
+        {
+            ConnectionAddress = GetConnectionAddressByName(connectionName);
+        }
+
+        internal static string GetConnectionAddressByName(string connectionName)
+        {
+            return ConfigurationManager.ConnectionStrings[connectionName].ConnectionString;
+        }
+
+        protected string ConnectionAddress { get; }
+
+
+        private static DataTable BuildDataTable(NpgsqlCommand command)
+        {
+            DataSet container = new DataSet();
+            using (var adapter = new NpgsqlDataAdapter(command))
+            {
+                adapter.Fill(container);
+            }
+            return container.Tables[0];
+        }
+
+        private static List<T> BuildObjectList<T>(NpgsqlCommand command, Func<IDataReader, T> objectBuilder)
+        {
+            List<T> list = new List<T>();
+
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    list.Add(objectBuilder(reader));
+                }
+            }
+
+            return list;
+        }
+
+        protected List<T> ExecuteQueryByString<T>(string sqlAsString, Func<IDataReader, T> objectBuilder)
+        {
+            return ExecuteQuery(ConnectionAddress, sqlAsString, command => BuildObjectList(command, objectBuilder));
+        }
+
+        public void ExecuteNonQueryByString(string sqlAsString)
+        {
+            ExecuteQuery(ConnectionAddress, sqlAsString, command => command.ExecuteNonQuery());
+        }
+
+        // ReSharper disable once TooManyArguments
+        private static T ExecuteQuery<T>(string connectionAddress, string sqlAsString, Func<NpgsqlCommand, T> action)
+        {
+            using (var connection = new NpgsqlConnection(connectionAddress))
+            {
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = sqlAsString;
+                    command.CommandType = CommandType.Text;
+
+                    connection.Open();
+
+                    return action(command);
+                }
+            }
+        }
+
+        protected static T? ReadNullableValue<T>(IDataReader reader, string columnName)
+            where T : struct
+        {
+            return (T?)(reader.IsDBNull(reader.GetOrdinal(columnName))
+                ? null
+                : reader[columnName]);
+        }
+
+        protected static string ReadString(IDataReader reader, string columnName)
+        {
+            return reader.IsDBNull(reader.GetOrdinal(columnName))
+                ? null
+                : reader[columnName].ToString();
+        }
+
+        public static NpgsqlConnection OpenDatabaseConnection(string connectionName)
+        {
+            var connectionSettings = ConfigurationManager.ConnectionStrings[connectionName];
+            return OpenConnectionFromAddress(connectionSettings.ConnectionString);
+        }
+
+        private static NpgsqlConnection OpenConnectionFromAddress(string connectionAddress)
+        {
+            var connection = new NpgsqlConnection(connectionAddress);
+            connection.Open();
+            return connection;
+        }
+    }
+}
